0ca7560c1c81c36bc2c5c95155ba97ab25db2512
thealamdev <shahalam141072@gmail.com>
2024-09-25 16:39:17 +0600

    Modification has been done in thealamdev

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,60 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="scroll-smooth">
+
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="This website is about manage events.">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>{{ config('app.name', 'Event Metro') }}</title>
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+        <!-- Style css  !-->
+        <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/nice-select2.css') }}">
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        {{-- <link href="https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,100..900&display=swap" rel="stylesheet"> --}}
+        @livewireStyles
+        @yield('style')
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
+    <!-- Sidenav start -->
+    @include('layouts.partials.sidebar')
+    <!-- Sidenav end -->
 
-            <!-- Page Content -->
-            <main>
+    <main class="w-full bg-white md:w-[calc(100%-256px)] md:ml-64 min-h-screen transition-all main">
+        <!-- Navbar Start -->
+        @include('layouts.partials.navbar')
+        <!-- Navbar End -->
+        <div class="pt-8 px-10">
+
+            <!-- Breadcrumb Start -->
+            @include('layouts.partials.breadcrumb')
+            <!-- Breadcrumb End -->
+
+            <div class="{{ Route::is('*.index') ? 'px-0' : 'md:px-12 sm:px-0' }} mt-5">
                 {{ $slot }}
-            </main>
+            </div>
         </div>
+
+    </main>
+
+    {{-- <script src="https://unpkg.com/@popperjs/core@2"></script> --}}
+    <script src="{{ asset('assets/js/script.js') }}"></script>
+    <script src="{{ asset('assets/js/custom.js') }}"></script>
+    <script src="{{ asset('assets/js/nice-select2.js') }}"></script>
+    <script>
+        var options = {
+            searchable: true,
+            placeholder: 'select',
+            searchtext: 'zoek',
+            selectedtext: 'item selected'
+        };
+        var instance = NiceSelect.bind(document.getElementById("seachable-select"), options);
+    </script>
+    @livewireScripts
+    @yield('script')
+
     </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LocalizationController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,7 +10,7 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified','locale'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -17,4 +18,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+require __DIR__ . '/sajal.php';
+require __DIR__ . '/thealamdev.php';
+
+Route::get('locale/{lang}', [LocalizationController::class, 'locale'])->name('local');
