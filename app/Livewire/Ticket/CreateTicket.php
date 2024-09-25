e72122b7aa1c203f694f29b3107f5dd9c3105fa3
thealamdev <shahalam141072@gmail.com>
2024-09-25 22:44:08 +0600

    Some modification has been done

--- app/Livewire/Ticket/CreateTicket.php
+++ app/Livewire/Ticket/CreateTicket.php
@@ -1,13 +1,51 @@
 <?php
 
-                namespace App\Livewire\Ticket;
+namespace App\Livewire\Ticket;
 
-                use Livewire\Component;
+use App\Models\Category;
+use App\Models\RequesterType;
+use App\Models\Source;
+use App\Models\Team;
+use Livewire\Component;
 
-                class CreateTicket extends Component
-                {
-                    public function render()
-                    {
-                        return view('livewire.ticket.create-ticket');
-                    }
-                }
+class CreateTicket extends Component
+{
+    /**
+     * Define public property $requester_type;
+     * @var array|object
+     */
+    public $requester_type;
+
+    /**
+     * Define public property $sources;
+     * @var array|object
+     */
+    public $sources;
+
+    /**
+     * Define public property $categories;
+     * @var array|object
+     */
+    public $categories;
+
+        /**
+     * Define public property $teams;
+     * @var array|object
+     */
+    public $teams;
+
+    /**
+     * Define public method mount() to load the resourses
+     */
+    public function mount(): void
+    {
+        $this->requester_type = RequesterType::query()->get();
+        $this->sources = Source::query()->get();
+        $this->categories = Category::query()->get();
+        $this->teams = Team::query()->get();
+    }
+    public function render()
+    {
+        return view('livewire.ticket.create-ticket');
+    }
+}
