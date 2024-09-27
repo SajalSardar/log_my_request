1fa66092d98bec2ea27c976a6c52d1f788edc44f
sajalsardar <sajal2.stars@gmail.com>
2024-09-27 22:29:34 +0600

    bug

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,29 +4,33 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 
-class AuthenticatedSessionController extends Controller
-{
+class AuthenticatedSessionController extends Controller {
     /**
      * Display the login view.
      */
-    public function create(): View
-    {
+    public function create(): View {
         return view('auth.login');
     }
 
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): RedirectResponse
-    {
+    public function store(LoginRequest $request) {
         $request->authenticate();
 
         $request->session()->regenerate();
+
+        $loginUser = Auth::user()->load('roles');
+
+        $role = $loginUser->roles->pluck('name')->toArray();
+
+        $request->session()->put('login_role', $role[0]);
 
         return redirect()->intended(route('dashboard', absolute: false));
     }
@@ -34,8 +38,7 @@ class AuthenticatedSessionController extends Controller
     /**
      * Destroy an authenticated session.
      */
-    public function destroy(Request $request): RedirectResponse
-    {
+    public function destroy(Request $request): RedirectResponse {
         Auth::guard('web')->logout();
 
         $request->session()->invalidate();
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,116 @@
 <x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+    <section class="bg-white border border-slate-400 lg:rounded-3xl sm:rounded-none">
+        <div class="lg:grid sm:h-[850px] h-[500px] lg:grid-cols-12">
+            <section
+                class="relative sm:hidden md:hidden lg:block xl:block flex h-full items-end lg:col-span-5 lg:h-full xl:col-span-6">
+                <img alt="" src="{{ asset('assets/images/login-page-image.png') }}"
+                    class="absolute lg:rounded-l-3xl sm:rounded-none inset-0 h-full w-full object-cover opacity-80" />
+            </section>
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+            <main class="flex items-center justify-center py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
+                <div class="max-w-xl lg:max-w-3xl border border-slate-200 rounded-lg p-12">
+                    <h3 class="font-bold text-2xl">SIGN IN</h3>
+                    <x-auth-session-status class="mb-4" :status="session('status')" />
+                    <form method="POST" action="{{ route('login') }}" class="mt-8 grid grid-cols-6 gap-6">
+                        @csrf
+                        <div class="col-span-6 sm:col-span-3">
+                            <a href="#" class="inline-block border border-slate-200 rounded-lg p-3 w-full">
+                                <img class="inline-block" src="{{ asset('assets/icons/goolge-logo.png') }}"
+                                    alt="google">
+                                <span class="pl-2 sm:hidden md:inline-block text-center">Sign in with Google</span>
+                                <span class="pl-2 md:hidden">Sign In</span>
+                            </a>
+                        </div>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                        <div class="col-span-6 sm:col-span-3">
+                            <a href="#" class="inline-block border border-slate-200 rounded-lg p-3 w-full">
+                                <img class="inline-block" src="{{ asset('assets/icons/facebook-logo.png') }}"
+                                    alt="facebook">
+                                <span class="pl-2 sm:hidden md:inline-block text-center">Sign in with Facebook</span>
+                                <span class="pl-2 md:hidden">Sign In</span>
+                            </a>
+                        </div>
+
+                        <div class="col-span-6 text-center">
+                            <span>Or Email</span>
+                        </div>
+
+                        <div class="col-span-6">
+                            <div class="col-span-6">
+                                <x-forms.text-input-icon type="email" name="email" placeholder="Email Address"
+                                    :value="old('email')" dir="start">
+                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <g clip-path="url(#clip0_1_4773)">
+                                            <path
+                                                d="M4 4H20C21.1 4 22 4.9 22 6V18C22 19.1 21.1 20 20 20H4C2.9 20 2 19.1 2 18V6C2 4.9 2.9 4 4 4Z"
+                                                stroke="#666666" stroke-width="1.5" stroke-linecap="round"
+                                                stroke-linejoin="round" />
+                                            <path d="M22 6L12 13L2 6" stroke="#666666" stroke-width="1.5"
+                                                stroke-linecap="round" stroke-linejoin="round" />
+                                        </g>
+                                        <defs>
+                                            <clipPath id="clip0_1_4773">
+                                                <rect width="24" height="24" fill="white" />
+                                            </clipPath>
+                                        </defs>
+                                    </svg>
+                                </x-forms.text-input-icon>
+                            </div>
+                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                        </div>
+
+                        <div class="col-span-6">
+                            <x-forms.text-input-icon type="password" name="password" placeholder="Password"
+                                dir="start">
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M19 11H5C3.89543 11 3 11.8954 3 13V20C3 21.1046 3.89543 22 5 22H19C20.1046 22 21 21.1046 21 20V13C21 11.8954 20.1046 11 19 11Z"
+                                        stroke="#666666" stroke-width="1.5" stroke-linecap="round"
+                                        stroke-linejoin="round" />
+                                    <path
+                                        d="M7 11V7C7 5.67392 7.52678 4.40215 8.46447 3.46447C9.40215 2.52678 10.6739 2 12 2C13.3261 2 14.5979 2.52678 15.5355 3.46447C16.4732 4.40215 17 5.67392 17 7V11"
+                                        stroke="#666666" stroke-width="1.5" stroke-linecap="round"
+                                        stroke-linejoin="round" />
+                                </svg>
+                            </x-forms.text-input-icon>
+                            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                        </div>
+
+                        <div class="col-span-6 flex justify-between">
+                            <p class="flex gap-4">
+                                <x-forms.checkbox-input name="remember" />
+                                <span class="text-sm text-gray-700">
+                                    {{ __('Remember me') }}
+                                </span>
+                            </p>
+                            <p class="flex gap-4">
+                                <span class="text-sm text-gray-700">
+                                    <a href="{{ route('password.request') }}">{{ __('Forget Password ?') }}</a>
+                                </span>
+                            </p>
+                        </div>
+
+                        <div class="col-span-6">
+                            <x-buttons.primary class="w-full">
+                                SIGN IN
+                            </x-buttons.primary>
+                        </div>
+
+
+                        <div class="col-span-6">
+                            <p class="mt-4 text-sm text-gray-500 sm:mt-0">
+                                Don't have an account?
+                                <a href="{{ route('register') }}" class="text-primary-400 underline font-bold">Sign
+                                    Up</a>.
+                            </p>
+                        </div>
+
+                    </form>
+                </div>
+            </main>
         </div>
+    </section>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
 </x-guest-layout>
