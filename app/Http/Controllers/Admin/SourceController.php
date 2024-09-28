eec2b02453e80f097eac6768e2d46551ee8341f2
sajalsardar <sajal2.stars@gmail.com>
2024-09-29 01:11:54 +0600

    menu

--- app/Http/Controllers/Admin/SourceController.php
+++ app/Http/Controllers/Admin/SourceController.php
@@ -2,81 +2,82 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use App\Models\Source;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Gate;
 
 class SourceController extends Controller {
     /**
      * Display a listing of the resource.
      */
     public function index() {
-        Gate::authorize( 'view', Source::class );
-        $sources = Cache::remember( 'source_list', 60 * 60, function () {
+        Gate::authorize('view', Source::class);
+        $sources = Cache::remember('source_list', 60 * 60, function () {
             return Source::get();
-        } );
+        });
 
-        return view( "source.index", compact( 'sources' ) );
+        return view("source.index", compact('sources'));
     }
 
     /**
      * Display a listing of the data table resource.
      */
     public function displayListDatatable() {
-        Gate::authorize( 'view', Source::class );
+        Gate::authorize('view', Source::class);
 
-        $source = Cache::remember( 'source_list', 60 * 60, function () {
+        $source = Cache::remember('source_list', 60 * 60, function () {
             return Source::get();
-        } );
+        });
     }
 
     /**
      * Show the form for creating a new resource.
      */
     public function create() {
-        Gate::authorize( 'create', Source::class );
-        return view( 'source.create' );
+        Gate::authorize('create', Source::class);
+        return view('source.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store( Request $request ) {
+    public function store(Request $request) {
         //
-        Gate::authorize( 'create', Source::class );
+        Gate::authorize('create', Source::class);
 
     }
 
     /**
      * Display the specified resource.
      */
-    public function show( Source $source ) {
+    public function show(Source $source) {
         //
-        Gate::authorize( 'view', $source );
-        return view( 'source.show' );
+        Gate::authorize('view', $source);
+        return view('source.show');
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit( Source $source ) {
-        Gate::authorize( 'update', $source );
-        return view( 'source.edit' );
+    public function edit(Source $source) {
+        Gate::authorize('update', $source);
+
+        return view('source.edit', compact('source'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update( Request $request, Source $source ) {
-        Gate::authorize( 'update', $source );
+    public function update(Request $request, Source $source) {
+        Gate::authorize('update', $source);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy( Source $source ) {
-        Gate::authorize( 'delete', $source );
+    public function destroy(Source $source) {
+        Gate::authorize('delete', $source);
     }
 }
--- app/Livewire/Source/CreateSource.php
+++ app/Livewire/Source/CreateSource.php
@@ -3,8 +3,9 @@
 namespace App\Livewire\Source;
 
 use App\Models\Source;
-use Livewire\Component;
+use Illuminate\Support\Facades\Gate;
 use Livewire\Attributes\Validate;
+use Livewire\Component;
 
 class CreateSource extends Component {
     #[Validate]
@@ -20,18 +21,22 @@ class CreateSource extends Component {
     }
 
     public function save() {
+        Gate::authorize('create', Source::class);
+
         $this->validate();
 
-        Source::create( [
+        Source::create([
             "title"  => $this->title,
             "status" => $this->status,
-        ] );
+        ]);
 
-        flash()->success( 'source Created!' );
-        return redirect()->to( '/dashboard/source' );
+        flash()->success('source Created!');
+        return redirect()->to('/dashboard/source');
     }
 
     public function render() {
-        return view( 'livewire.source.create-source' );
+        Gate::authorize('create', Source::class);
+
+        return view('livewire.source.create-source');
     }
 }
