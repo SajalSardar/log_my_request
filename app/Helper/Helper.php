ae6d017877b09fc56b629bf10a99cc2f3a503962
sajalsardar <sajal2.stars@gmail.com>
2024-09-30 00:30:12 +0600

    status

--- app/Helper/Helper.php
+++ app/Helper/Helper.php
@@ -21,15 +21,15 @@ class Helper {
      * @return string
      */
     public static function status(?string $status): string {
-        if ($status === '1') {
+        if ($status == '1') {
             return ' <span class="inline-flex items-center bg-green-100 text-white text-xs font-normal px-2.5 py-0.5 rounded-full dark:bg-green-600 dark:text-green-300">
                         <span class="w-2 h-2 me-1 bg-green-500 rounded-full"></span>
-                        Available
+                        Active
                     </span>';
         } else {
             return '<span class="inline-flex items-center bg-red-100 text-white text-xs font-medium px-2.5 py-0.5 rounded-full dark:bg-red-600 dark:text-red-300">
                         <span class="w-2 h-2 me-1 bg-red-500 rounded-full"></span>
-                        Unavailable
+                        Inactive
                     </span>';
         }
     }
