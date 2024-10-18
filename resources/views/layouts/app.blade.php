cea387071ec9f396f39e1ee7d02bb59f19d9927d
sajalsardar <sajal2.stars@gmail.com>
2024-10-18 23:48:42 +0600

    js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,11 +41,10 @@
 
 </main>
 @livewireScripts
-{{-- <script src="https://unpkg.com/@popperjs/core@2"></script> --}}
-<!-- <script src="{{ asset('assets/js/jquery-3.7.1.slim.min.js') }}"></script> -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+<script src="{{ asset('assets/js/jquery.min.js') }}"></script>
 <script src="{{ asset('assets/js/select2.min.js') }}"></script>
-<script src="https://cdn.ckeditor.com/ckeditor5/36.0.1/classic/ckeditor.js"></script>
+<script src="{{ asset('assets/js/ckeditor.js') }}"></script>
 <script src="{{ asset('vendor/datatable/dataTables.min.js') }}"></script>
 <script src="{{ asset('assets/js/script.js') }}"></script>
 <script src="{{ asset('assets/js/custom.js') }}"></script>
