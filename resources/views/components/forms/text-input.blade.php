102073c14e89f1176eb2bacb0513ce2c179a65d7
Shah Alam <shahalam141072@gmail.com>
2024-11-01 22:24:44 +0600

    Login and registration has been done

--- resources/views/components/forms/text-input.blade.php
+++ resources/views/components/forms/text-input.blade.php
@@ -1,3 +1,3 @@
 <input style="height: 40px;" {!! $attributes->merge([
-    'class' => 'w-full border border-slate-400 focus:border-primary-400 rounded-lg bg-transparent text-title',
+    'class' => 'w-full border border-slate-400 focus:border-primary-400 rounded-lg bg-transparent text-paragraph',
 ]) !!}>
