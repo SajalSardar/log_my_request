4fecd12b130dd994aacc14328cbac9ffd159f7b4
sajalsardar <sajal2.stars@gmail.com>
2024-11-05 21:40:23 +0600

    merge

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,7 +14,7 @@
 
     <!-- Scripts -->
     {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
-    <link rel="stylesheet" href="{{ asset('build/assets/app-D2D16GT0.css') }}">
+    <link rel="stylesheet" href="{{ asset('build/assets/app-RLrkd56A.css') }}">
     @livewireStyles
 </head>
 
