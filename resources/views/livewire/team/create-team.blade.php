e25c0c3a617353f9737ce97020f3cc1abbde5f6f
Shah Alam <shahalam141072@gmail.com>
2024-11-09 22:43:30 +0600

    Team create and update has been done

--- resources/views/livewire/team/create-team.blade.php
+++ resources/views/livewire/team/create-team.blade.php
@@ -1,110 +1,105 @@
-<section class="py-5">
-    <form wire:submit="save">
-        <div class="grid md:grid-cols-2 sm:grid-cols-1 sm:gap-1 md:gap-4">
-
-            <div class="p-2 w-full">
-                <x-forms.label for="form.name" required='yes'>
-                    {{ __('Team Name') }}
-                </x-forms.label>
-                <x-forms.text-input type="text" wire:model.blur='form.name' placeholder="Team name" />
-                <x-input-error :messages="$errors->get('form.name')" class="mt-2" />
-            </div>
-
+<form wire:submit="save">
+    <div class="flex justify-between" style="width:651px !important">
+        <h3 class="font-inter font-semibold text-[#333] text-[20px] mb-[24px]">Create Team</h3>
+        <div>
+            <a href="{{ route('admin.requestertype.index') }}" class="flex items-center px-0 bg-transparent gap-1 text-heading-light text-paragraph hover:text-primary-400 transition-colors">
+                Go to Team Lists
+                <svg class="w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M20 12H4" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M15 17C15 17 20 13.3176 20 12C20 10.6824 15 7 15 7" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+            </a>
         </div>
-        <div class="grid md:grid-cols-2 sm:grid-cols-1 sm:gap-1 md:gap-4">
+    </div>
 
-            <div class="p-2 w-full">
-                <x-forms.label for="form.image">
-                    {{ __('Team Image') }}
-                </x-forms.label>
-                <x-forms.text-input wire:model.blur="form.image" accept="jpg,png,jpeg" max='3024' type="file" />
-                <x-input-error :messages="$errors->get('form.image')" class="mt-2" />
-            </div>
-
-        </div>
-
-        <div class="grid md:grid-cols-2 sm:grid-cols-1 sm:gap-1 md:gap-4">
-
-            <div class="p-2 w-full">
-                <x-forms.label for="form.category_id" required='yes'>
-                    {{ __('Ticket Category') }}
-                </x-forms.label>
-
-                <div wire:ignore>
-                    <x-forms.select2-select wire:model.defer='form.category_id' id="category_id" multiple>
-                        <option value="" disabled>Select Category</option>
-                        @foreach ($categories as $each)
-                            <option value="{{ $each->id }}">{{ $each->name }}</option>
-                        @endforeach
-                    </x-forms.select2-select>
-                    <x-input-error :messages="$errors->get('form.category_id')" class="mt-2" />
+    <div class="flex flex-row">
+        <div style="width:651px !important">
+            <div class="border border-base-500 p-5 rounded">
+                <div class="grid lg:grid-cols-1 md:grid-cols-1 sm:grid-cols-1 sm:gap-1 md:gap-2">
+                    <div class="pb-2 w-full">
+                        <x-forms.label for="form.name" required='yes'>
+                            {{ __('Team Name') }}
+                        </x-forms.label>
+                        <x-forms.text-input type="text" wire:model.blur='form.name' placeholder="Team name" />
+                        <x-input-error :messages="$errors->get('form.name')" class="mt-2" />
+                    </div>
                 </div>
-
-            </div>
-
-
-        </div>
-
-        <div class="grid md:grid-cols-2 sm:grid-cols-1 sm:gap-1 md:gap-4">
-
-            <div class="p-2 w-full">
-                <x-forms.label for="form.department_id" required='yes'>
-                    {{ __('Department') }}
-                </x-forms.label>
-                <div wire:ignore>
-                    <x-forms.select2-select wire:model.defer='form.department_id' id="department_id">
-                        <option value="">Select Department</option>
-                        @foreach ($departments as $department)
-                            <option value="{{ $department->id }}">{{ $department->name }}</option>
-                        @endforeach
-                    </x-forms.select2-select>
-
-                    <x-input-error :messages="$errors->get('form.department_id')" class="mt-2" />
+                <div class="grid lg:grid-cols-1 md:grid-cols-1 sm:grid-cols-1 sm:gap-1 md:gap-2">
+                    <div class="pt-2 w-full">
+                        <x-forms.label for="form.category_id" required='yes'>
+                            {{ __('Request Category') }}
+                        </x-forms.label>
+                        <div wire:ignore>
+                            <x-forms.select2-select wire:model.defer='form.category_id' id="category_id" multiple>
+                                <option value="" disabled>Select Category</option>
+                                @foreach ($categories as $each)
+                                    <option value="{{ $each->id }}">{{ $each->name }}</option>
+                                @endforeach
+                            </x-forms.select2-select>
+                            <x-input-error :messages="$errors->get('form.category_id')" class="mt-2" />
+                        </div>
+                    </div>
+                </div>
+                <div class="grid lg:grid-cols-1 md:grid-cols-1 sm:grid-cols-1 sm:gap-1 md:gap-2">
+                    <div class="pt-2 w-full">
+                        <x-forms.label for="form.department_id" required='yes'>
+                            {{ __('Department') }}
+                        </x-forms.label>
+                        <div wire:ignore>
+                            <x-forms.select2-select wire:model.defer='form.department_id' id="department_id">
+                                <option value="">Select Department</option>
+                                @foreach ($departments as $department)
+                                    <option value="{{ $department->id }}">{{ $department->name }}</option>
+                                @endforeach
+                            </x-forms.select2-select>
+                            <x-input-error :messages="$errors->get('form.department_id')" class="mt-2" />
+                        </div>
+                    </div>
+                </div>
+                <div class="grid lg:grid-cols-1 md:grid-cols-1 sm:grid-cols-1 sm:gap-1 md:gap-2">
+                    <div class="pt-2 w-full">
+                        <x-forms.label for="form.agent_id">
+                            {{ __('Agent') }}
+                        </x-forms.label>
+                        <div wire:ignore>
+                            <x-forms.select2-select wire:model.defer='form.agent_id' id="agent_id" multiple>
+                                <option value="" disabled>Select agent</option>
+                                @foreach ($agentUser as $agent)
+                                    <option value="{{ $agent->id }}">{{ $agent->name }}</option>
+                                @endforeach
+                            </x-forms.select2-select>
+                            <x-input-error :messages="$errors->get('form.agent_id')" class="mt-2" />
+                        </div>
+                    </div>
+                </div>
+                <div class="grid lg:grid-cols-1 md:grid-cols-1 sm:grid-cols-1 sm:gap-1 md:gap-2">
+                    <div class="pt-2 w-full">
+                        <x-forms.label for="form.status" required='yes'>
+                            {{ __('Status') }}
+                        </x-forms.label>
+                        <x-forms.select-input wire:model.blur='form.status'>
+                            <option selected>Select Status</option>
+                            <option value="1">Active</option>
+                            <option value="0">Inactive</option>
+                        </x-forms.select-input>
+                        <x-input-error :messages="$errors->get('form.status')" class="mt-2" />
+                    </div>
+                </div>
+                <div class="grid lg:grid-cols-1 md:grid-cols-1 sm:grid-cols-1 sm:gap-1 md:gap-2">
+                    <div class="pt-2 w-full">
+                        <x-forms.label for="form.image">
+                            {{ __('Team Image') }}
+                        </x-forms.label>
+                        <x-forms.input-image wire:model.blur="form.image" accept="jpg,png,jpeg" max='3024' type="file" />
+                        <x-input-error :messages="$errors->get('form.image')" class="mt-2" />
+                    </div>
+                </div>
+                <div class="pt-6">
+                    <x-buttons.primary>
+                        Create
+                    </x-buttons.primary>
                 </div>
             </div>
-
         </div>
-        <div class="grid md:grid-cols-2 sm:grid-cols-1 sm:gap-1 md:gap-4">
-
-            <div class="p-2 w-full">
-                <x-forms.label for="form.agent_id">
-                    {{ __('Agent') }}
-                </x-forms.label>
-                <div wire:ignore>
-                    <x-forms.select2-select wire:model.defer='form.agent_id' id="agent_id" multiple>
-                        <option value="" disabled>Select agent</option>
-                        @foreach ($agentUser as $agent)
-                            <option value="{{ $agent->id }}">{{ $agent->name }}</option>
-                        @endforeach
-                    </x-forms.select2-select>
-
-                    <x-input-error :messages="$errors->get('form.agent_id')" class="mt-2" />
-                </div>
-            </div>
-
-        </div>
-
-        <div class="grid md:grid-cols-2 sm:grid-cols-1 sm:gap-1 md:gap-4">
-
-            <div class="p-2 w-full">
-                <x-forms.label for="form.status" required='yes'>
-                    {{ __('Status') }}
-                </x-forms.label>
-                <x-forms.select-input wire:model.blur='form.status'>
-                    <option selected>Select Status</option>
-                    <option value="1">Active</option>
-                    <option value="0">Inactive</option>
-                </x-forms.select-input>
-                <x-input-error :messages="$errors->get('form.status')" class="mt-2" />
-            </div>
-
-        </div>
-
-        <div class="p-2">
-            <x-buttons.primary>
-                Create Team
-            </x-buttons.primary>
-        </div>
-
-    </form>
-</section>
+    </div>
+</form>
--- resources/views/team/create.blade.php
+++ resources/views/team/create.blade.php
@@ -1,32 +1,13 @@
 <x-app-layout>
     @section('title', 'Create Team')
     @section('breadcrumb')
-        <x-breadcrumb>
-            Create Team
-        </x-breadcrumb>
+    <x-breadcrumb>
+        Create Team
+    </x-breadcrumb>
     @endsection
-    <div class="p-5 rounded-lg shadow-lg">
-        <header class="py-5 px-2 grid md:grid-cols-2 sm:grid-cols-1 md:gap-1 sm:gap-1">
-            <div class="infos">
-                <h3 class="font-bold text-xl">Create Team</h3>
-                <p>Please fill the input field where sign <span class="text-red-500">(*) </span> have.</p>
-            </div>
-
-            <div class="flex md:flex-row-reverse sm:flex-row">
-                <div>
-                    <x-actions.href href="{{ route('admin.team.index') }}">
-                        {{ __('Team List') }}
-                    </x-actions.href>
-                </div>
-            </div>
-        </header>
-        <hr>
-        <livewire:team.create-team :categories="$categories" :agentUser="$agentUser" :departments="$departments" />
-
-    </div>
-
+    <livewire:team.create-team :categories="$categories" :agentUser="$agentUser" :departments="$departments" />
     <script>
-        document.addEventListener('DOMContentLoaded', function() {
+        document.addEventListener('DOMContentLoaded', function () {
             initSelect2form('category_id');
             initSelect2form('agent_id');
             initSelect2form('department_id');
