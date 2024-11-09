02217a69c85760f57faaa0235e5d8c894b3e9fd8
sajalsardar <sajal2.stars@gmail.com>
2024-11-09 19:09:15 +0600

    bug

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,6 @@
 
 use App\Http\Controllers\LocalizationController;
 use App\Http\Controllers\ProfileController;
-use App\Models\User;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,7 +9,6 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    $users = User::with('roles')->where('id', Auth::id())->first();
     return view('dashboard');
 })->middleware(['auth', 'verified', 'locale'])->name('dashboard');
 
