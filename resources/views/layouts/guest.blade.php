92280ecee3d13cdb6923a00d484b4c556c92e2ae
sajalsardar <sajal2.stars@gmail.com>
2024-11-27 22:37:55 +0600

    build

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,7 +14,7 @@
 
     <!-- Scripts -->
     {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
-    <link rel="stylesheet" href="{{ asset('build/assets/app-e6tQnOOw.css') }}">
+    <link rel="stylesheet" href="{{ asset('build/assets/app-BOA-QMN0.css') }}">
     @livewireStyles
 </head>
 
